9c55224a66d337e732f85b4a0193b88ccd274d41
gary <garyyaoo@gmail.com>
2020-03-08 16:15:38 -0700

    button

--- src/components/NameInput.js
+++ src/components/NameInput.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import { TextField } from '@material-ui/core';
+import {
+    TextField, 
+    withStyles,
+ } from '@material-ui/core';
+
+const styles = theme => ({
+    root: {
+        border: '1px solid #e2e2e1',
+        overflow: 'hidden',
+        borderRadius: 8,
+        backgroundColor: '#e6e6e6',
+        '&:hover': {
+          backgroundColor: '#f2f2f2',
+        },
+        '&$focused': {
+          backgroundColor: '#f2f2f2',
+        },
+      },
+      focused: {},
+})
 
 class NameInput extends React.PureComponent {
     render() {
+        const { classes } = this.props;
+    
         return (
-            <TextField label="Summoner Name" variant="filled"/>
+            <TextField InputProps={{classes}} label="Summoner Name" variant='filled'/>
         );
     };
 }
 
-export default NameInput;
+export default withStyles(styles)(NameInput);
